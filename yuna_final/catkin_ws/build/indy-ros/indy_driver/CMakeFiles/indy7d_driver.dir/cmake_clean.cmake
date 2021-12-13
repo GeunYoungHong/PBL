69028ffae8999f6cf9c0bcae581ef47268374159
YunaChoi <chldbsk2220@hanyang.ac.kr>
2021-12-13 20:32:31 +0900

    update

---
+++ yuna_final/catkin_ws/build/indy-ros/indy_driver/CMakeFiles/indy7d_driver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/choi/catkin_ws/devel/lib/indy_driver/indy7d_driver"
+  "/home/choi/catkin_ws/devel/lib/indy_driver/indy7d_driver.pdb"
+  "CMakeFiles/indy7d_driver.dir/src/TrajectoryDownloader/JointTrajectoryDownloader.cpp.o"
+  "CMakeFiles/indy7d_driver.dir/src/indy7d_driver.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/indy7d_driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ yuna_final/catkin_ws/build/indy-ros/indyrp2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/choi/catkin_ws/src/indy-ros/indyrp2_description
+# Build directory: /home/choi/catkin_ws/build/indy-ros/indyrp2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
