3178b7963542daa6fb8e4b43d9810e0e78b9c4bd
YunaChoi <chldbsk2220@hanyang.ac.kr>
2021-12-13 20:16:42 +0900

    add catkin_ws

---
+++ yuna_final/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/choi/catkin_ws/src
+# Build directory: /home/choi/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("indy-ros/indy7_control")
+subdirs("indy-ros/indy7_description")
+subdirs("indy-ros/indy7_gazebo")
+subdirs("indy-ros/indy7_moveit_config")
+subdirs("indy-ros/indyrp2_description")
+subdirs("indy-ros/indyrp2_moveit_config")
+subdirs("indy-ros/indy_driver_py")
+subdirs("indy-ros/indy_driver")
---
+++ yuna_final/catkin_ws/build/indy-ros/indy_driver/CMakeFiles/indy6d_driver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/choi/catkin_ws/devel/lib/indy_driver/indy6d_driver"
+  "/home/choi/catkin_ws/devel/lib/indy_driver/indy6d_driver.pdb"
+  "CMakeFiles/indy6d_driver.dir/src/TrajectoryDownloader/JointTrajectoryDownloader.cpp.o"
+  "CMakeFiles/indy6d_driver.dir/src/indy6d_driver.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/indy6d_driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
